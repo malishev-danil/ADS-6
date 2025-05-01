79240b840f6328252571d755a310582f170ef610
malishev-danil <malishev-danil5@yandex.ru>
2025-05-01 14:23:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,8 +1,11 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+
 #include <iostream>
 #include <stdexcept>
+#include <string>
+
 template<typename T>
 class TPQueue {
 
